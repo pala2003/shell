faa3fcf3dd4a69bc2e38f240d38764756991c604
palash.bose <palash.bose.dsi@noreply.github.com>
2025-04-11 01:22:27 -0400

    Modified for Subderectories command

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -56,7 +56,7 @@ rm ./data/processed/user_logs/*ipaddr*
 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls ./data/processed >> ./data/inventory.txt
+ls -R ./data/processed >> ./data/inventory.txt
 
 
 ###########################################
